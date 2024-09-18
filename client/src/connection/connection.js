787c332830354432b2850da05c51eb7a8437b2f9
Sivaramakrishnan <sivaramakrishnan0327@gmail.com>
2024-09-18 10:23:48 +0530

    Updated

--- client/src/connection/connection.js
+++ client/src/connection/connection.js
@@ -1,3 +1,3 @@
-const baseURL = 'https://daily-tasks-backend-sigma.vercel.app'; 
+const baseURL = 'https://daily-tasks-backend-hjozr2ogw-sivaramakrishnans-projects.vercel.app'; 
 // const baseURL = 'http://localhost:5000';
 export default baseURL;
