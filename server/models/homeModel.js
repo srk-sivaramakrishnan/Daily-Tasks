eca38bbff6b90253ac0765d759b44a1e82ae6041
Sivaramakrishnan <sivaramakrishnan0327@gmail.com>
2024-09-20 08:56:37 +0530

    Updated

--- server/models/homeModel.js
+++ server/models/homeModel.js
@@ -96,7 +96,7 @@ const markIncompleteTasks = async () => {
 
 
 // Schedule the cron job to run at 11:59 PM every day
-cron.schedule('53 21 * * *', markIncompleteTasks, {
+cron.schedule('00 09 * * *', markIncompleteTasks, {
   timezone: 'Asia/Kolkata', // Specify your timezone
 });
 
